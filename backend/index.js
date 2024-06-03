cd9907340742f8ad2f3bc30403e6a9f23d0f7474
Aurelien Destailleur <aurelien.destailleur@quadradiffusion2.onmicrosoft.com>
2024-06-03 20:25:42 +0200

    recup

---
+++ backend/index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const sqlite3 = require('sqlite3').verbose()
+const cors = require('cors');
+
+// Autorisation CORS headers
+app.use(cors());
+
+
+// Connexcion BDD
+let db = new sqlite3.Database('./db/pokeqd.db', (err) => {
+    if (err) {
+        return console.error(err.message)
+    }
+    console.log('Database connected')
+})
+
+// >> CREATE TABLLE <<
+let query = 'CREATE TABLE IF NOT EXISTS pokemon (ID NUMBER, name CHAR(255), type CHAR(255), hability CHAR(255));'
+
+db.run(query)
+
+db.close((err) => {
+    if (err) {
+        console.log(err.message)
+    }
+})
+
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`app running on ${port}`)
+})
+
+// > Route API <<< 
+
+// GET ALL
+app.get('/get/all/pokemon', (req, res) => {
+    // Recuperation des datas
+    fetch('https://pokeapi.co/api/v2/pokemon', {})
+        .then(res => res.json())
+        .then(data => {
+            for (const elem in data) {
+                console.log(elem)
+            }
+            res.json(data.results)
+        })
+})
+
+// GET BY ID
+app.get('/get/pokemon/:id', (req, res) => {
+    const { id } = req.params
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {})
+    .then(res => res.json())
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+// UDPDATE
+
+app.put('/update/pokemon/:id', (req, res) => {
+    const { id } = req.params; // Recup id via parama
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {})
+    .then(res => res.json())
+    .then(data => {
+        // update here
+        console.log('data into update back', data);
+        // utilisation de serialize pour mettre a jour les données
+        db.serialize(() => {
+            const query = `UPDATE pokemon SET name = ?, type = ?, hability = ? WHERE ID = ?`
+            // Une fois la query preparer, on passe les params de mise a jour
+            db.run(query, [data.name, data.type, data.hability, id], (err) => {
+                if (err) {
+                    console.error(err.message);
+                    res.status(500).json({ message: 'Failed to update Pokemon' });
+                } else {
+                    res.json({ message: 'Pokemon updated successfully' });
+                }
+            })
+        })
+        res.json({ "message" : "Updated" });
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
